21eef101f8fa1cf806980633b6612bd65d7c2bbd
rodydeep <121086606+rodydeep@users.noreply.github.com>
2022-12-29 15:29:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,6 @@ with st.expander('Analyze Text'):
         st.write('Polarity: ', round(blob.sentiment.polarity,2))
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
 
-
-    pre = st.text_input('Clean Text: ')
-    if pre:
-        st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
-                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
-
 with st.expander('Analyze CSV'):
     upl = st.file_uploader('Upload file')
 
